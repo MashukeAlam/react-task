f4df01158b64c7dcce46c4f5f46b516a6560dedd
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-06-17 09:26:32 +0600

    Added the map functions to show the list in the table

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
 
@@ -8,13 +8,13 @@ const Problem1 = () => {
     const [status, setStatus] = useState('');
     const [list, setList] = useState([]);
 
-    const handleClick = (val) =>{
+    const handleClick = (val) => {
         setShow(val);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // console.log('here');
+        console.log('here', todo, status);
         const id = list.length + 1;
         setList((now) => [
             ...now,
@@ -23,7 +23,8 @@ const Problem1 = () => {
                 todo,
                 status
             }
-        ])
+        ]);
+        console.log(list);
     }
 
 
@@ -36,10 +37,10 @@ const Problem1 = () => {
                 <div className="col-6 ">
                     <form className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name" onChange={(e) => setTodo(e.target.value)}/>
+                            <input type="text" className="form-control" placeholder="Name" onChange={(e) => setTodo(e.target.value)} />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status" onChange={(e) => setTodo(e.target.value)}/>
+                            <input type="text" className="form-control" placeholder="Status" onChange={(e) => setStatus(e.target.value)} />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
@@ -49,25 +50,33 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>Completed</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {list.map(item => (
+                                <tr onClick={() => setPopup({ trigger: true, id: item.id - 1 })} className='bg-emerald-200 hover:bg-emerald-300 hover:font-medium font-light' key={item.id}>
+                                    <td>{item.todo}</td>
+                                    <td>{item.status}</td>
+
+                                </tr>
+                            ))}
+
+
                         </tbody>
                     </table>
                 </div>
